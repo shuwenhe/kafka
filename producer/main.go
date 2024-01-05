572eca9454091295cae58c7d23d4bc1572d5e4f0
Shuwen He <1201220707@pku.edu.cn>
2024-01-05 11:20:44 +0800

    feat: kafka

--- producer/main.go
+++ producer/main.go
@@ -40,31 +40,27 @@ ProducerLoop:
 			fmt.Println("Interrupt signal received, shutting down...")
 			break ProducerLoop
 		default:
+			var wg sync.WaitGroup
 			countCurrentLimit := 10
 			workQueue := make(chan int, countCurrentLimit)
-			for i := 300; i <= 399; i++ { // 模拟生成100个数据
+			for i := 0; i <= countCurrentLimit; i++ { // 模拟生成100个数据
 				input := fmt.Sprintf("Data %d", i)
 				message := &sarama.ProducerMessage{
 					Topic: "mqtt",
 					Value: sarama.StringEncoder(input),
 				}
-
-				var wg sync.WaitGroup
-				for i := 0; i < countCurrentLimit; i++ {
-					wg.Add(1)
-					go func(i int, workQueue chan int, msg *sarama.ProducerMessage) { // 使用 go 协程发送消息，以避免阻塞主循环
-						select {
-						case producer.Input() <- msg:
-							fmt.Println("Message sent to partition", msg.Partition)
-						case err := <-producer.Errors():
-							fmt.Println("Failed to produce message:", err)
-						}
-					}(i, workQueue, message)
-				}
+				wg.Add(1)
+				go func(i int, workQueue chan int, msg *sarama.ProducerMessage) { // 使用 go 协程发送消息，以避免阻塞主循环
+					select {
+					case producer.Input() <- msg:
+						fmt.Println("Message sent to partition", msg.Partition)
+					case err := <-producer.Errors():
+						fmt.Println("Failed to produce message:", err)
+					}
+				}(i, workQueue, message)
 			}
 		}
 	}
-
 	wg.Wait()
 	fmt.Println("Producer shutting down.")
 }
